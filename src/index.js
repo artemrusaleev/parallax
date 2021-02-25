4796886363457fe5aa34f0e1e84b98aa95a5b460
Artem <31157501+artemrusaleev@users.noreply.github.com>
2021-02-25 16:43:27 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
+
+import ParallaxContainer from "./ParallaxContainer";
+
+/* Credit to Online Tutorials
+  https://www.youtube.com/watch?v=dc4zyX6DuOc
+*/
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+function App() {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      {/*
+        Available props:
+          backgroundImage - Ex: import backgroundImage from "./assets/background.jpg",
+          layers - Ex: const layers = [
+                        {
+                          name: "text",
+                          image: import textImage from "./assets/text.png,
+                          dataDepth: "0.1"
+                        },
+                        {
+                          name: "astronaut",
+                          image: import astronautImage from "./assets/astronaut.png,
+                          dataDepth: "2"
+                        }
+                      ];
+      */}
+      <ParallaxContainer />
+    </React.Fragment>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
